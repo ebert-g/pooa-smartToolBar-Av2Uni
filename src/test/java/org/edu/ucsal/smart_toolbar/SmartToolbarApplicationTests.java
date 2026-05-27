7111cfc8ba88fb3a76bcbfb95f5212808bbdc93b
Ebert dos Santos Guerreiro <ebertg.est@gmail.com>
2026-05-26 23:10:12 -0300

    initialize project with Maven, add main application files and basic structure

---
+++ src/test/java/org/edu/ucsal/smart_toolbar/SmartToolbarApplicationTests.java
@@ -0,0 +1,13 @@
+package org.edu.ucsal.smart_toolbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartToolbarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
